2f40781146708bfaaf2680bf1a276cf41bc6da78
Ankush Saini <145102317+AnkushSaini22@users.noreply.github.com>
2025-03-18 10:56:32 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 )
 
 func main() {
-	// Get the current time
+	
 	currentTime := time.Now()
-
-	// Print the current date and time
 	fmt.Println("Current Date & Time:", currentTime.Format("2006-01-02 15:04:05"))
 }
